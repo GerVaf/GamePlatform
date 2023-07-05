ffbf23ad2d590242e496d9d83298e7a81a568226
GerVaf <thantzinmin667@gmail.com>
2023-07-05 22:30:26 +0630

    v3.8

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -39,31 +39,30 @@ const Detail = () => {
   return (
     <>
       <div className="flex">
-
         {/* right side  */}
-        <div className="w-12/12 flex flex-col gap-5">
+        <div className="sm:w-12/12 w-[100vw] flex flex-col gap-5">
           {/* top */}
-          <div className=" flex text-white gap-5">
+          <div className=" flex flex-col sm:flex-row text-white gap-5">
             {/* left  */}
-            <div className="w-6/12 rounded-lg">
+            <div className="sm:w-6/12 rounded-lg">
               <img
-                className=" w-[100%] h-96 object-top object-cover"
+                className=" sm:w-[100%] w-[100vw] sm:h-96 object-top object-cover"
                 src={gameData?.background_image}
                 alt=""
               />
             </div>
 
             {/* right  */}
-            <div className="flex flex-col gap-5 w-6/12">
+            <div className="sm:w-6/12 w-[100%] flex flex-col gap-5 ">
               <p
-                className="text-xl font-bold
+                className="sm:text-xl font-bold
               "
               >
                 {gameData?.name_original}
               </p>
 
               {/* platform  */}
-              <div className="flex text-xl gap-5">
+              <div className="flex sm:text-xl gap-5">
                 {gameData?.parent_platforms.find((el) =>
                   el.platform.name.includes("PC")
                 ) && (
@@ -86,7 +85,7 @@ const Detail = () => {
                 )}
               </div>
 
-              <p className=" h-72 overflow-y-scroll scrollbar-thumb-sky-600 scrollbar-thin scrollbar-track-slate-600">
+              <p className=" sm:h-72 h-44 w-[100vw] overflow-y-scroll scrollbar-thumb-sky-600 scrollbar-thin scrollbar-track-slate-600">
                 {gameData?.description_raw}
               </p>
             </div>
@@ -96,27 +95,30 @@ const Detail = () => {
           <div className="text-white flex flex-col gap-5">
             {/* gametype */}
             <div className="flex flex-col gap-2">
-              <p className="text-lg font-bold">GAME TYPE</p>
-              <div className="flex gap-3 px-3 py-1 rounded-sm mainbg">
+              <p className="sm:text-lg font-bold">GAME TYPE</p>
+              <div className="flex flex-wrap justify-start items-center gap-3 px-3 py-1 rounded-sm mainbg">
                 {gameData?.genres.map((genr) => {
                   return (
-                    <span key={genr.id} className="text-md border-r-2 px-3">
+                    <span
+                      key={genr.id}
+                      className="sm:text-md border-r-2 px-1 sm:text-md text-[10px] sm:px-3"
+                    >
                       {genr.name}
                     </span>
                   );
                 })}
               </div>
             </div>
-            
+
             {/* added customer */}
-                <div className="flex flex-col gap-2">
-                  <p className="text-lg font-bold">ADDED CUSTOMER COUNT</p>
-                  <span className="">{gameData?.added}</span>
-                </div>
+            <div className="flex flex-col gap-2">
+              <p className="sm:text-lg font-bold">ADDED CUSTOMER COUNT</p>
+              <span className="sm:text-md text-[10px]">{gameData?.added}</span>
+            </div>
 
             {/* avaiable platform  */}
             <div className="flex flex-col gap-2">
-              <p className="text-lg font-bold">SIMILAR GAMES</p>
+              <p className="sm:text-lg font-bold">SIMILAR GAMES</p>
               <div className="flex  gap-3  text-md">
                 <Trailer stores={gameData?.stores} />
               </div>
@@ -128,13 +130,16 @@ const Detail = () => {
               <div className="flex flex-wrap gap-3  text-md">
                 {gameData?.developers.map((dev) => {
                   return (
-                    <div className="flex h-48 rounded-t-md flex-col gap-2 bg-white font-bold text-slate-600 items-center" key={dev.id}>
+                    <div
+                      className="flex sm:h-48 h-32 rounded-t-md flex-col gap-2 bg-zinc-500 font-bold text-slate-200 items-center"
+                      key={dev.id}
+                    >
                       <img
-                        className="  rounded-t-md w-[100%] h-36 object-cover"
+                        className="  rounded-t-md w-[100%] h-24 sm:h-36 object-cover"
                         src={dev.image_background}
                         alt=""
-                        />
-                        <p>{dev.name}</p>
+                      />
+                      <p className="text-[10px] sm:text-md">{dev.name}</p>
                     </div>
                   );
                 })}
@@ -142,10 +147,10 @@ const Detail = () => {
             </div>
 
             {/* tags */}
-            <div className="flex gap-3 flex-wrap">
+            <div className="flex gap-3 flex-wrap mb-24">
               {gameData?.tags.map((tag) => {
                 return (
-                  <button className="px-3 py-1 rounded-sm mainbg ">
+                  <button className="px-3 py-1 text-[10px] sm:text-md rounded-sm mainbg ">
                     {tag.name}
                   </button>
                 );
--- src/components/Detail/Trailer.jsx
+++ src/components/Detail/Trailer.jsx
@@ -4,16 +4,16 @@ import Marquee from "react-fast-marquee";
 const Trailer = ({ stores }) => {
   return (
     <Marquee
-      className="flex h-72 "
+      className="flex sm:h-72  h-32"
       speed={70}
       pauseOnHover={true}
       gradientColor={false}
     >
       {stores?.map((sto) => {
         return (
-          <div key={sto.id} className=" mx-5 flex  gap-3 font-bold">
+          <div key={sto.id} className=" mx-5 flex  sm:gap-3 font-bold">
             <img
-              className=" h-56 object-cover rounded-md"
+              className=" sm:h-56 h-32 object-cover rounded-md"
               src={sto.store.image_background}
               alt=""
             />
